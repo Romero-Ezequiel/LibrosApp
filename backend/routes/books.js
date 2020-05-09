3d2f2687ccabe9d6b7a93d444ac4bbcc439edbda
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-05-09 18:55:50 -0300

    rutas de la aplicación

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -17,8 +17,25 @@ router.get('/', async (req, res)=>{
 
 
 router.post('/', async (req, res)=>{
-    console.log(req.body);
-    res.send('Recibido');
+    //console.log(req.body);
+    const {titulo, autor, id} = req.body
+    const newBook = new Book({
+        titulo, autor, id
+    })
+    //Guardo el nuevo libro en la base de datos
+    await newBook.save();
+    console.log(newBook);
+    //res.send('Recibido');
+    res.send({mensaje: 'Libro guardado'})
 });
+
+router.delete('/:id', async (req, res)=>{
+    //console.log(req.params.id);
+   // res.send('Libro eliminado de la Base de datos');
+    const book = await Book.findByIdAndDelete(req.params.id);
+    console.log(book);
+    res.send('Libro eliminado de la Base de datos');
+
+})
 
 module.exports = router;
