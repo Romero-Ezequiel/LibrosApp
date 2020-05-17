15c23197edbc52ec271656c25911e8c1c20fb1b3
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-05-16 21:04:04 -0300

    Rutas del servidor

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,7 +2,8 @@
 
 const { Router } = require('express');
 const router = Router();
-
+const fs = require('fs-extra');
+const path = require('path');
 const Book = require('../models/Books');
 
 // //Creo mis rutas con router
@@ -15,16 +16,15 @@ router.get('/', async (req, res)=>{
     res.json(books);
 });
 
-
 router.post('/', async (req, res)=>{
     //console.log(req.body);
-    const {titulo, autor, id} = req.body
-    const newBook = new Book({
-        titulo, autor, id
-    })
+    const { titulo, autor, id } = req.body;
+    const imagePath = '/img/' + req.file.filename;
+
+    const newBook = new Book({titulo, autor, id, imagePath});
     //Guardo el nuevo libro en la base de datos
     await newBook.save();
-    console.log(newBook);
+    //console.log(newBook);
     //res.send('Recibido');
     res.send({mensaje: 'Libro guardado'})
 });
@@ -33,9 +33,9 @@ router.delete('/:id', async (req, res)=>{
     //console.log(req.params.id);
    // res.send('Libro eliminado de la Base de datos');
     const book = await Book.findByIdAndDelete(req.params.id);
-    console.log(book);
+   // console.log(book);
+    fs.unlink(path.resolve('./backend/public/' + book.imagePath));
     res.send('Libro eliminado de la Base de datos');
-
 })
 
 module.exports = router;
